c9c3cd1848103bbba44351c811d3c0fc1ef1da3a
William Law <wlaw@stanford.edu>
2018-08-22 05:45:30 -0700

    Iniitial module files for nero

---
+++ anaconda/3.lua
@@ -0,0 +1,10 @@
+whatis("Name:        Anaconda3")
+whatis("Version:     5.2")
+whatis("Category:    applications, interpreters, languagesx")
+whatis("Keywords:    application, interpreter, language")
+whatis("URL:         https://www.anaconda.com/download/#linux")
+whatis("Description: Python 3 distribution")
+
+prepend_path("PATH","/share/sw/open/anaconda/3/bin")
+setenv("CONDA_PYTHON_EXE","/share/sw/open/anaconda/3/bin/python")
+prepend_path("PYTHONPATH","/share/sw/open/anaconda/3/bin")
