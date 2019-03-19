cead108b4b8d6e03e3f79c5c5f3c341c3932d8ca
Neal Soderquist <neals@stanford.edu>
2019-03-19 16:46:05 -0700

    Adding spm12

---
+++ spm/12.lua
@@ -0,0 +1,8 @@
+whatis("Name:        SPM")
+whatis("Version:     12")
+whatis("Category:    applications, interpreters, toolbox, languages")
+whatis("Keywords:    application, interpreter, toolbox, language")
+whatis("URL:         https://www.fil.ion.ucl.ac.uk/spm/software/spm12/")
+whatis("Description: The SPM software package is a toolbox for Matlab that has been designed for the analysis of brain imaging data sequences. The sequences can be a series of images from different cohorts, or time-series from the same subject. The current release is designed for the analysis of fMRI, PET, SPECT, EEG and MEG.")
+
+setenv("spm_path","/share/sw/restricted/matlab/MATLAB-R2018b/toolbox/spm12")
