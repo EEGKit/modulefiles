cb0251420c821d305cad621cca4077d6fe657a0c
Christina Doane <cdoane@stanford.edu>
2019-03-22 13:03:40 -0700

    add lua file for mricrogl

---
+++ mricrogl/1.0.20180623.lua
@@ -0,0 +1,8 @@
+whatis("Name:        mricrogl")
+whatis("Version:     1.0.20180623")
+whatis("Category:    applications, neuroimaging")
+whatis("Keywords:    application, neuroimaging")
+whatis("URL:         https://www.nitrc.org/frs/?group_id=889")
+whatis("Description: MRIcroGL allows you to view 2D slices and renderings of your brain imaging data. The software should be able to read most medical images, including those in the following formats: NIfTI (.nii, .nii.gz, .hdr/.img), Bio-Rad Pic (.pic), NRRD, Philips (.par/.rec), ITK MetaImage (.mhd, .mha), AFNI (.head/.brik), Freesurfer (.mgh, .mgz), basic DICOM (extensions vary)")
+ 
+prepend_path("PATH","/share/sw/open/mricrogl/mricrogl_lx/")
