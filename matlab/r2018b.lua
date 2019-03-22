f241d27c9f9698ef2ceb1e24f23d2b1b7c632580
Neal Soderquist <neals@stanford.edu>
2019-03-22 10:10:42 -0700

    adding group controls to matlab modulefile

--- matlab/r2018b.lua
+++ matlab/r2018b.lua
@@ -5,8 +5,12 @@ whatis("Keywords:    application, interpreter, language")
 whatis("URL:         http://www.mathworks.com/products/matlab")
 whatis("Description: A multi-paradigm numerical computing environment and programming language.")
 
-setenv("TZ","America/Los_Angeles")
-setenv("MATLABDIR","/share/sw/restricted/matlab/MATLAB-R2018b:/share/sw/restricted/matlab/MATLAB-R2018b/toolbox")
-
-prepend_path("PATH","/share/sw/restricted/matlab/MATLAB-R2018b/bin")
-prepend_path("PYTHONPATH","/share/sw/restricted/matlab/MATLAB-R2018b")
+local grps = capture("groups | grep nero_swmatlab")
+if grps ~= "" then
+  setenv("TZ","America/Los_Angeles")
+  setenv("MATLABDIR","/share/sw/restricted/matlab/MATLAB-R2018b:/share/sw/restricted/matlab/MATLAB-R2018b/toolbox")
+  prepend_path("PATH","/share/sw/restricted/matlab/MATLAB-R2018b/bin")
+  prepend_path("PYTHONPATH","/share/sw/restricted/matlab/MATLAB-R2018b")
+else
+  LmodMessage("You are not authorized to load this module. If you need access contact srcc-support@stanford.edu for more information.")
+end
