1be15b10f91679dadde5323ecb454ef8bb10a2d4
Neal Soderquist <neals@stanford.edu>
2019-03-26 14:33:31 -0700

    Adding Stata15

---
+++ stata/15.lua
@@ -0,0 +1,9 @@
+whatis("Name:        Stata")
+whatis("Version:     15")
+whatis("Category:    applications, statistics, licensed")
+whatis("Keywords:    application, statistics")
+whatis("URL:         https://www.stata.com/why-use-stata")
+whatis("Description: Integrated statistical software package for data analysis, data management, and graphics.")
+
+prepend_path("PATH","/share/sw/restricted/stata/stata15")
+prepend_path("LD_LIBRARY_PATH","/share/sw/open/zlib/1.2.3/lib:/share/sw/open/libpng/1.6.2/lib")
