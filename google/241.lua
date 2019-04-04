5b237ba905106f8ec0e938a9f9403c844a23c427
Mr. Addis Walter O'Connor <addiso@stanford.edu>
2019-04-03 22:00:43 -0700

    Update Module file for Latest Google SDK 241.lua

---
+++ google/241.lua
@@ -0,0 +1,6 @@
+whatis("Name:        Google Cloud Utilities")
+whatis("Version:     233")
+whatis("URL:         https://cloud.google.com/sdk")
+whatis("Description: tools for working with google cloud")
+
+prepend_path("PATH","/share/sw/open/google/google-cloud-sdk-233.0.0/bin")
