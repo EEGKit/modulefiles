c31a467c19d3619410e0d40db3a243e72bd8ed69
William Law <wlaw@stanford.edu>
2019-04-15 10:43:17 -0700

    Added module for oracle-cx-client and sqlplus

---
+++ sqlplus/18.5.0.0.lua
@@ -0,0 +1,9 @@
+whatis("Name:        SQL*PLUS")
+whatis("Version:     18.5.0.0")
+whatis("Category:    applications, sql")
+whatis("Keywords:    oracle, sql")
+whatis("URL:         https://www.oracle.com/technetwork/topics/linuxx86-64soft-092277.html")
+whatis("Description: SQL*PLUS client for oracle databases")
+
+prepend_path("PATH","/share/sw/open/sqlplus/18.5.0.0/bin")
+prepend_path("LD_LIBRARY_PATH","/share/sw/open/sqlplus/18.5.0.0/lib")
