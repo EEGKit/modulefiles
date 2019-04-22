0a84439d858f49385666dd6b559d5d6bdec2b370
Neal Soderquist <neals@stanford.edu>
2019-04-22 13:12:33 -0700

    adding mriqc

---
+++ mriqc/0.14.2.lua
@@ -0,0 +1,9 @@
+whatis("Name:        mriqc")
+whatis("Version:     0.14.2")
+whatis("Category:    applications, neuroimaging")
+whatis("Keywords:    application, neuroimaging")
+whatis("URL:         https://mriqc.readthedocs.io")
+whatis("Description: MRIQC provides a series of image processing workflows to extract and compute a series of NR, IQMs, to be used in QAPs for MRI.")
+ 
+prepend_path("PATH","/share/sw/open/mriqc/:/share/sw/open/mriqc/bin")
+always_load("anaconda/3")
