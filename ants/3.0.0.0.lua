1acf7d2ed2cff6c2bd5d088e25a3935517240028
Neal Soderquist <neals@stanford.edu>
2019-04-23 10:55:40 -0700

    Adding ANTs 3.0.0.0

---
+++ ants/3.0.0.0.lua
@@ -0,0 +1,9 @@
+whatis("Name:        ants")
+whatis("Version:     3.0.0.0")
+whatis("Category:    applications, neuroimaging")
+whatis("Keywords:    application, neuroimaging")
+whatis("URL:         https://stnava.github.io/ANTs")
+whatis("Description: ANTs is a state-of-the-art medical image registration and segmentation toolkit.")
+
+setenv("ANTSPATH","/share/sw/open/ants/bin") 
+prepend_path("PATH","/share/sw/open/ants/bin")
