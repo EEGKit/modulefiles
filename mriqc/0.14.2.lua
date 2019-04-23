cd39f62e0c87c25f697d7d09451c98f555757b55
Neal Soderquist <neals@stanford.edu>
2019-04-23 11:06:03 -0700

    Updated mriqc to include ants

--- mriqc/0.14.2.lua
+++ mriqc/0.14.2.lua
@@ -6,4 +6,4 @@ whatis("URL:         https://mriqc.readthedocs.io")
 whatis("Description: MRIQC provides a series of image processing workflows to extract and compute a series of NR, IQMs, to be used in QAPs for MRI.")
 
 prepend_path("PATH","/share/sw/open/mriqc/:/share/sw/open/mriqc/bin")
-always_load("anaconda/3")
+always_load("anaconda/3","ants/3.0.0.0")
