02b0dfa9060220bcc0a4f02c9cfb1ffec493693f
William Law <wlaw@stanford.edu>
2019-04-24 12:02:11 -0700

    Added in cuda driver 418

---
+++ cuda-driver/418.lua
@@ -0,0 +1,8 @@
+whatis("Name:        CUDA Driver")
+whatis("Version:     418")
+whatis("Category:    driver")
+whatis("Keywords:    driver")
+whatis("URL:         https://www.nvidia.com")
+whatis("Description: driver libraries for NVIDIA CUDA")
+
+prepend_path("LD_LIBRARY_PATH","LD_LIBRARY_PATH=/share/sw/open/cuda/drivers/418/lib64")
