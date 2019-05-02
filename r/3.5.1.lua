b1f28dcf866dacf99238eacc8a6b02ab19bd1cfc
Neal Soderquist <neals@stanford.edu>
2019-05-02 13:42:28 -0700

    Added r/3.5.1.lua

---
+++ r/3.5.1.lua
@@ -0,0 +1,6 @@
+whatis("Name: R")
+whatis("Version: 3.5.1")
+whatis("Category: tools")
+whatis("URL: http://cran.cnr.berkeley.edu/")
+whatis("Description: R")
+prepend_path("PATH", "/share/sw/open/R/R-3.5.1/bin")
