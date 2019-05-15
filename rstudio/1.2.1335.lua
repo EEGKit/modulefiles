328f059c03edfbbe7bc8abe22d8519dabd1b9db6
Neal Soderquist <neals@stanford.edu>
2019-05-15 15:11:46 -0700

    Creating rstudio lua

---
+++ rstudio/1.2.1335.lua
@@ -0,0 +1,8 @@
+whatis("Name:        RStudio Server")
+whatis("Version:     1.2.1335")
+whatis("Category:    applications")
+whatis("Keywords:    application, IDE, R")
+whatis("URL:         https://www.rstudio.com/products/RStudio/")
+whatis("Description: An integrated development environment for R.")
+
+prepend_path("PATH","/share/sw/open/rstudio/1.2.1335/usr/lib/rstudio-server/bin")
