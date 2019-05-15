e2171e2fa25b900ee369b1c72a7f997dc6729f46
Neal Soderquist <neals@stanford.edu>
2019-05-15 15:16:04 -0700

    Adding shiny

---
+++ shiny/1.1.0.lua
@@ -0,0 +1,8 @@
+whatis("Name:        Shiny Server")
+whatis("Version:     1.1.0")
+whatis("Category:    applications")
+whatis("Keywords:    applications")
+whatis("URL:         https://shiny.rstudio.com/")
+whatis("Description: Shiny is an R package that makes it easy to build interactive web apps straight from R. ")
+
+prepend_path("PATH","/share/sw/open/shiny/opt/shiny-server/bin")
