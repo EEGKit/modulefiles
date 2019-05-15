630b000a6904e84412261fc050ff80062fdea6ae
Neal Soderquist <neals@stanford.edu>
2019-05-15 11:15:28 -0700

    adding tigervnc/1.9.0

---
+++ tigervnc/1.9.0.lua
@@ -0,0 +1,8 @@
+whatis("Name:        tigervnc")
+whatis("Version:     1.9.0")
+whatis("Category:    applications, platform")
+whatis("Keywords:    application, platform")
+whatis("URL:         https://tigervnc.org/")
+whatis("Description: TigerVNC is a platform-neutral implementation of VNC, a client/server application that allows users to launch and interact with graphical applications on remote machines.")
+
+prepend_path("PATH","/share/sw/open/tigervnc/usr/bin")
