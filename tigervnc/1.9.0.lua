6b7a092a701f8c09fe5c29229545224b33db7c90
Neal Soderquist <neals@stanford.edu>
2019-05-15 11:19:15 -0700

    updates to tigervnc path

--- tigervnc/1.9.0.lua
+++ tigervnc/1.9.0.lua
@@ -5,4 +5,4 @@ whatis("Keywords:    application, platform")
 whatis("URL:         https://tigervnc.org/")
 whatis("Description: TigerVNC is a platform-neutral implementation of VNC, a client/server application that allows users to launch and interact with graphical applications on remote machines.")
 
-prepend_path("PATH","/share/sw/open/tigervnc/usr/bin")
+prepend_path("PATH","/share/sw/open/tigervnc/tigervnc-1.9.0/usr/bin")
