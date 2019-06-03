fa77e2cb7974e70b2c615013ea9e7be097c27eec
Neal Soderquist <neals@stanford.edu>
2019-06-03 13:33:33 -0700

    Adding nk2edf

---
+++ nk2edf/14.lua
@@ -0,0 +1,8 @@
+whatis("Name:        nk2edf")
+whatis("Version:     14")
+whatis("Category:    applications")
+whatis("Keywords:    application, converter")
+whatis("URL:         https://www.teuniz.net/edf/")
+whatis("Description: nk2edf converts EEG-files in Nihon Kohden® format to the European Data Format including the annotations/events.")
+
+prepend_path("PATH","/share/sw/open/nk2edf/")
