df19fb33d5e19307435c5c52fa574da2efd86bad
William Law <wlaw@stanford.edu>
2019-06-12 22:01:41 -0700

    Fixed cuda-driver issue; added new module for cuda 10.1

--- cuda-driver/418.lua
+++ cuda-driver/418.lua
@@ -5,4 +5,4 @@ whatis("Keywords:    driver")
 whatis("URL:         https://www.nvidia.com")
 whatis("Description: driver libraries for NVIDIA CUDA")
 
-prepend_path("LD_LIBRARY_PATH","LD_LIBRARY_PATH=/share/sw/open/cuda/drivers/418/lib64")
+prepend_path("LD_LIBRARY_PATH","/share/sw/open/cuda/drivers/418/lib64")
---
+++ cuda/10.1.lua
@@ -0,0 +1,8 @@
+whatis("Name:        CUDA")
+whatis("Version:     10.1")
+whatis("Category:    runtime")
+whatis("Keywords:    runtime")
+whatis("URL:         https://www.nvidia.com")
+whatis("Description: CUDA 10.1")
+
+prepend_path("LD_LIBRARY_PATH","/share/sw/open/cuda/10.1/lib64")
