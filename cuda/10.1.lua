378e9404b6c930c0daeaba9dc1e5bb16726bb797
William Law <wlaw@stanford.edu>
2019-06-12 22:03:09 -0700

    Made cuda 10.1 module load cuda-driver

--- cuda/10.1.lua
+++ cuda/10.1.lua
@@ -5,4 +5,5 @@ whatis("Keywords:    runtime")
 whatis("URL:         https://www.nvidia.com")
 whatis("Description: CUDA 10.1")
 
+always_load("cuda-driver")
 prepend_path("LD_LIBRARY_PATH","/share/sw/open/cuda/10.1/lib64")
