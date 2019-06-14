13e5120614f68d55d628b80f2e89e8ec44d11159
William Law <wlaw@stanford.edu>
2019-06-14 15:02:24 -0700

    i can't write if thens if my life depends on it

--- google/250.lua
+++ google/250.lua
@@ -4,6 +4,3 @@ whatis("URL:         https://cloud.google.com/sdk")
 whatis("Description: tools for working with google cloud")
 
 prepend_path("PATH","/share/sw/open/google/google-cloud-sdk-250.0.0/bin")
-#if `which python` ~= "" then
-#  always_load("anaconda")
-#else
