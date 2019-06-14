191649147d67dca806371e8815fa9381c79466ba
William Law <wlaw@stanford.edu>
2019-06-14 14:54:06 -0700

    google 250 added

--- google/250.lua
+++ google/250.lua
@@ -1,7 +1,7 @@
 whatis("Name:        Google Cloud Utilities")
-whatis("Version:     244")
+whatis("Version:     250")
 whatis("URL:         https://cloud.google.com/sdk")
 whatis("Description: tools for working with google cloud")
 
-prepend_path("PATH","/share/sw/open/google/google-cloud-sdk-244.0.0/bin")
+prepend_path("PATH","/share/sw/open/google/google-cloud-sdk-250.0.0/bin")
 #always_load("anaconda/2")
