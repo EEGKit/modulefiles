18c57255794c491fe6e2c2ac4387df40448197e1
William Law <wlaw@stanford.edu>
2019-06-14 15:26:10 -0700

    removed anaconda from google/241

--- google/241.lua
+++ google/241.lua
@@ -4,4 +4,3 @@ whatis("URL:         https://cloud.google.com/sdk")
 whatis("Description: tools for working with google cloud")
 
 prepend_path("PATH","/share/sw/open/google/google-cloud-sdk-233.0.0/bin")
-always_load("anaconda/2")
