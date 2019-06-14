6675eca8b360545463de1e9697c4083cbe99c804
William Law <wlaw@stanford.edu>
2019-06-14 14:47:45 -0700

    Added 244 module for google

---
+++ google/244.lua
@@ -0,0 +1,7 @@
+whatis("Name:        Google Cloud Utilities")
+whatis("Version:     244")
+whatis("URL:         https://cloud.google.com/sdk")
+whatis("Description: tools for working with google cloud")
+
+prepend_path("PATH","/share/sw/open/google/google-cloud-sdk-244.0.0/bin")
+#always_load("anaconda/2")
