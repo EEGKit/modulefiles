25a86114241d73ae885abb2343adc1ec8e1114c7
William Law <wlaw@stanford.edu>
2019-07-05 14:38:43 -0700

    added singularity

---
+++ singularity/3.2.1.lua
@@ -0,0 +1,6 @@
+whatis("Name:        Singularity")
+whatis("Version:     3.2.1")
+whatis("URL:         https://sylabs.io")
+whatis("Description: Singularity container runtime")
+
+prepend_path("PATH","/share/sw/open/singularity/3.2.1/bin")
