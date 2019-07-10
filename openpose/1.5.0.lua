5d4ad5bc0c83de24e57963ad806bd03572e28275
Christina Doane <cdoane@stanford.edu>
2019-07-10 13:36:09 -0700

    Update openpose/1.5.0.lua

--- openpose/1.5.0.lua
+++ openpose/1.5.0.lua
@@ -5,5 +5,5 @@ whatis("Keywords:    library, video, images")
 whatis("URL:         https://github.com/CMU-Perceptual-Computing-Lab/openpose/")
 whatis("Description: library for real-time multi-person keypoint detection and multi-threading written in C++ using OpenCV and Caffe*.")
 
-setenv("OPENPOSE_HOME","/share/sw/open/openpose/openpose_sw")
+setenv("OPENPOSE_HOME","/share/sw/open/openpose/openpose_sw/openpose_orig")
 prepend_path("LD_LIBRARY_PATH","/share/sw/open/openpose/openpose-lib-dependencies")
