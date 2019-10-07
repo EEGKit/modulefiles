39a9078adc9eb3c0fbaa5fda0fbc9e356a833e0d
Neal Soderquist <neals@stanford.edu>
2019-10-07 14:33:43 -0700

    Update ukbgene/1.28.19.lua, ukbfetch/1.30.19.lua files

---
+++ ukbfetch/1.30.19.lua
@@ -0,0 +1,8 @@
+whatis("Name:        ukbfetch")
+whatis("Version:     1.30.19")
+whatis("Category:    applications")
+whatis("Keywords:    application, ukbiobank")
+whatis("URL:         https://www.ukbiobank.ac.uk/")
+whatis("Description: Download tool used to download approved bulk files from UK BioBank.")
+
+prepend_path("PATH","/share/sw/open/ukbiobank/ukbfetch")
---
+++ ukbgene/1.28.19.lua
@@ -0,0 +1,8 @@
+whatis("Name:        ukbgene")
+whatis("Version:     1.28.19")
+whatis("Category:    applications")
+whatis("Keywords:    application, ukbiobank")
+whatis("URL:         https://www.ukbiobank.ac.uk/")
+whatis("Description: Download tool used to download approved genetic data from UK BioBank.")
+
+prepend_path("PATH","/share/sw/open/ukbiobank/ukbgene")
