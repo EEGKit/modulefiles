eb9bf0bb49a5b70a331688fc59f7932b82df663e
Neal Soderquist <neals@stanford.edu>
2019-10-10 11:26:46 -0700

    Update sbt/1.3.2.lua

---
+++ sbt/1.3.2.lua
@@ -0,0 +1,8 @@
+whatis("Name:        sbt")
+whatis("Version:     1.3.2")
+whatis("Category:    build tool")
+whatis("Keywords:    build tool, tool")
+whatis("URL:         https://www.scala-sbt.org")
+whatis("Description: The interactive build tool used to define your tasks in Scala. Run them in parallel from sbt's interactive shell.")
+
+prepend_path("PATH","/share/sw/open/sbt/bin/")
