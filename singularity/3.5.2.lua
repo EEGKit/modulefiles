10040c0f2f65c84d8a38bed793e8258bdf55972f
Neal Soderquist <neals@stanford.edu>
2020-02-03 11:57:19 -0800

    Update singularity/3.5.2.lua

---
+++ singularity/3.5.2.lua
@@ -0,0 +1,6 @@
+whatis("Name:        Singularity")
+whatis("Version:     3.5.2")
+whatis("URL:         https://sylabs.io")
+whatis("Description: Singularity container runtime")
+
+prepend_path("PATH","/share/sw/open/singularity/3.5.2/bin")
