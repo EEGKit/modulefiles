2b5fbdc4e91fe5cebe7d21a44b0ff44a44219cce
Neal Soderquist <neals@stanford.edu>
2020-05-01 15:32:22 -0700

    Update elastix/5.0.0.lua

--- elastix/5.0.0.lua
+++ elastix/5.0.0.lua
@@ -6,3 +6,4 @@ whatis("URL:         https://github.com/SuperElastix/elastix")
 whatis("Description: elastix 5.0.0: a toolbox for rigid and nonrigid registration of images.")
 
 prepend_path("LD_LIBRARY_PATH","/share/sw/open/elastix/5.0.0/lib")
+prepend_path("PATH","/share/sw/open/elastix/5.0.0/bin")
