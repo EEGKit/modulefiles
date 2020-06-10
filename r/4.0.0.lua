e9701de22cfd8ce06076573153405c33eea0e8c9
Neal Soderquist <neals@stanford.edu>
2020-06-10 16:51:21 -0700

    Deleted r/4.0.0.lua

--- r/4.0.0.lua
+++
@@ -1,6 +0,0 @@
-whatis("Name: R")
-whatis("Version: 4.0.0")
-whatis("Category: tools")
-whatis("URL: http://cran.cnr.berkeley.edu/")
-whatis("Description: R")
-prepend_path("PATH", "/share/sw/open/r/4.0.0/bin")
