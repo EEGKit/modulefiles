f536076cd96eb44d1bb2cfe1e6ee33eedea1a3eb
Neal Soderquist <neals@stanford.edu>
2020-06-10 15:25:01 -0700

    adding r 4.0

---
+++ r/4.0.0.lua
@@ -0,0 +1,7 @@
+whatis("Name: R")
+whatis("Version: 4.0.0")
+whatis("Category: tools")
+whatis("URL: http://cran.cnr.berkeley.edu/")
+whatis("Description: R")
+prepend_path("PATH", "/share/sw/open/r/4.0.0/bin")
+always_load("anaconda/3")
