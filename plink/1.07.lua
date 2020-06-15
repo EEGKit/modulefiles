8d28f3093a3d60cd7be115bc99019a65ceae4bba
Neal Soderquist <neals@stanford.edu>
2020-06-15 16:01:31 -0700

    Adding plink

---
+++ plink/1.07.lua
@@ -0,0 +1,7 @@
+whatis("Name:        plink")
+whatis("Version:     1.07")
+whatis("Category:    biology, genomics")
+whatis("URL:         http://zzz.bwh.harvard.edu/plink/")
+whatis("Description: PLINK is a free, open-source whole genome association analysis toolset, designed to perform a range of basic, large-scale analyses in a computationally efficient manner.")
+
+prepend_path("PATH","/share/sw/open/plink/1.07/bin")
