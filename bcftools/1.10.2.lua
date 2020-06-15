7d5136197901f449d37833432915eb0a69ad2aae
Neal Soderquist <neals@stanford.edu>
2020-06-15 16:50:24 -0700

    adding bcftools

---
+++ bcftools/1.10.2.lua
@@ -0,0 +1,8 @@
+whatis("Name:        bcftools")
+whatis("Version:     1.10.2")
+whatis("Category:    biology, genomics")
+whatis("URL:         https://github.com/samtools/bcftools")
+whatis("Description: BCFtools is a program for variant calling and manipulating files in the Variant Call Format (VCF) and its binary counterpart BCF.")
+
+prepend_path("PATH","/share/sw/open/bcftools/1.10.2/bin")
+prepend_path("MANPATH","/share/sw/open/bcftools/1.10.2/share/man")
