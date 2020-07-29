3f5221771012d57dddccd6b5e4cced678222187e
William Law <wlaw@stanford.edu>
2020-07-29 09:29:53 -0700

    Added google sdk 303

---
+++ google/303.lua
@@ -0,0 +1,6 @@
+whatis("Name:        Google Cloud Utilities")
+whatis("Version:     303")
+whatis("URL:         https://cloud.google.com/sdk")
+whatis("Description: tools for working with google cloud")
+
+prepend_path("PATH","/share/sw/open/google/google-cloud-sdk-303.0.0/bin")
