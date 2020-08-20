799f0448640ea9ba95ee15b38bc2f631a99a5d61
Neal Soderquist <neals@stanford.edu>
2020-08-20 14:24:17 -0700

    Update stata-mp/16.1.lua

---
+++ stata-mp/16.1.lua
@@ -0,0 +1,9 @@
+whatis("Name:        Stata/MP")
+whatis("Version:     16.1")
+whatis("Category:    applications, statistics, licensed")
+whatis("Keywords:    application, statistics")
+whatis("URL:         https://www.stata.com/why-use-stata")
+whatis("Description: Integrated statistical software package for data analysis, data management, and graphics.")
+
+prepend_path("PATH","/share/sw/restricted/stata/stata16/stata16_mp")
+prepend_path("LD_LIBRARY_PATH","/share/sw/open/zlib/1.2.3/lib:/share/sw/open/libpng/1.6.2/lib")
