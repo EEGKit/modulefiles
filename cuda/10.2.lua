3209ba2fb618fa3255d7e86432ce10339974a9b1
William Law <wlaw@stanford.edu>
2020-10-14 16:39:53 -0700

    removed pound

--- cuda/10.2.lua
+++ cuda/10.2.lua
@@ -5,5 +5,4 @@ whatis("Keywords:    runtime")
 whatis("URL:         https://www.nvidia.com")
 whatis("Description: CUDA 10.2")
 
-#always_load("cuda-driver")
 prepend_path("LD_LIBRARY_PATH","/share/sw/open/cuda/10.2/lib64")
