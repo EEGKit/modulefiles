b2baa0160ca3c85d613185785d2bbba06101c83b
William Law <wlaw@stanford.edu>
2020-10-14 16:31:14 -0700

    added 10.2

---
+++ cuda/10.2.lua
@@ -0,0 +1,9 @@
+whatis("Name:        CUDA")
+whatis("Version:     10.2")
+whatis("Category:    runtime")
+whatis("Keywords:    runtime")
+whatis("URL:         https://www.nvidia.com")
+whatis("Description: CUDA 10.2")
+
+always_load("cuda-driver")
+prepend_path("LD_LIBRARY_PATH","/share/sw/open/cuda/10.2/lib64")
