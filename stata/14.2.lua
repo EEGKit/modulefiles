87d9e38c955bd69f56f021ad42b1c41d67b1fb36
Neal Soderquist <neals@stanford.edu>
2022-02-09 13:58:36 -0800

    Update 14.2.lua

--- stata/14.2.lua
+++ stata/14.2.lua
@@ -5,7 +5,7 @@ whatis("Keywords:    application, statistics")
 whatis("URL:         https://www.stata.com/why-use-stata")
 whatis("Description: Integrated statistical software package for data analysis, data management, and graphics.")
 
-local grps = capture("groups | grep nero_swstata")
+local grps = capture("groups | grep carina_swstata")
 if grps ~= "" then
   prepend_path("PATH","/share/sw/restricted/stata/stata14/14.2")
   prepend_path("LD_LIBRARY_PATH","/share/sw/open/zlib/1.2.3/lib:/share/sw/open/libpng/1.6.2/lib")
